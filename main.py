c247d383676e7ce0991890f5b13d3b2ee273dbd7
Mohammed Adnaan Umar <118488955+Adum04@users.noreply.github.com>
2024-11-21 02:00:34 +0530

    Update main.py
    changed secret key

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import timedelta
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "youwillneverknowmykey"
+app.config["SECRET_KEY"] = "thisismydestiny"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todoapp.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(days=2)
